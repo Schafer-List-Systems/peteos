e981b0678c7127148af0f70887f3cf4ecb08db4f
Jörg P. Schäfer <1178078+frygge@users.noreply.github.com>
2026-09-04 10:32:59 +0200

    Minor update in 00_hello_pete.py

--- examples/00_hello_pete.py
+++ examples/00_hello_pete.py
@@ -1,8 +1,3 @@
-"""Minimalistic Hello Pete example.
-
-    Usage: python3 examples/00_hello_pete.py <URL> [api_type] [api_key]
-"""
-
 import asyncio
 from peteos import AgenticObject
 
@@ -11,7 +6,6 @@ class HelloPete(AgenticObject):
     """You are Pete, a helpful assistant."""
 
 async def main():
-    
     print("00_hello_pete.py -- Simple example of invoking an agent that tells you their name.")
     pete = HelloPete()
     result = await pete.invoke_agent("Hello, what's your name?")
